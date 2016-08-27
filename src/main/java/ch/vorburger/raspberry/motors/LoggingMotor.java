9911147f3c2fd12e8a2d4cbec0ad4b255e4cbe56
Michael Vorburger <mike@vorburger.ch>
2016-08-28 00:31:36 +0200

    Don't log in LoggingMotor if the change would have no physical effect

--- src/main/java/ch/vorburger/raspberry/motors/LoggingMotor.java
+++ src/main/java/ch/vorburger/raspberry/motors/LoggingMotor.java
@@ -8,6 +8,9 @@ public class LoggingMotor extends Motor {
 	private static final Logger LOG = LoggerFactory.getLogger(Motor.class);
 	private final String name;
 
+	private boolean isEnabled = false;
+	private int speed = Integer.MAX_VALUE;
+
 	public LoggingMotor(String name) {
 		super();
 		this.name = name;
@@ -15,17 +18,26 @@ public class LoggingMotor extends Motor {
 
 	@Override
 	public void enable() {
-		LOG.info("enabled {} motor", name);
+		if (!isEnabled) {
+			LOG.info("enabled {} motor", name);
+			isEnabled = true;
+		}
 	}
 
 	@Override
 	public void disable() {
-		LOG.info("disabled {} motor", name);
+		if (!isEnabled) {
+			LOG.info("disabled {} motor", name);
+			isEnabled = false;
+		}
 	}
 
 	@Override
 	public void setSpeed(int speed) {
-		LOG.info("set speed {} on {} motor", speed, name);
+		if (this.speed != speed) {
+			LOG.info("set speed {} on {} motor", speed, name);
+			this.speed = speed;
+		}
 	}
 
 }
