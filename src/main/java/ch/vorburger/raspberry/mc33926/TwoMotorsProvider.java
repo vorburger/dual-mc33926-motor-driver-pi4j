00b86f9fc527b6b6481527bf0063d310a47374d7
Michael Vorburger <mike@vorburger.ch>
2016-10-06 15:31:44 +0200

    Remove un-used Guava and unncessarry inject deps, to easier bundling
    This slightly simplifies bundling this library into
    /home/vorburger/dev/RaspiRover/git/ch.vorburger.raspberry.mc33926

--- src/main/java/ch/vorburger/raspberry/mc33926/TwoMotorsProvider.java
+++ src/main/java/ch/vorburger/raspberry/mc33926/TwoMotorsProvider.java
@@ -1,7 +1,5 @@
 package ch.vorburger.raspberry.mc33926;
 
-import javax.inject.Provider;
-
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.RaspiBcmPin;
 import com.pi4j.io.gpio.RaspiGpioProvider;
@@ -12,27 +10,28 @@ import ch.vorburger.raspberry.motors.Motor;
 import ch.vorburger.raspberry.motors.ReverseMotor;
 import ch.vorburger.raspberry.motors.TwoMotors;
 
-public class TwoMotorsProvider implements Provider<TwoMotors> {
+public class TwoMotorsProvider /* implements Provider<TwoMotors> */ {
 
-	static private boolean gpioFactoryDefaultProviderInitialized = false;
+    static private boolean gpioFactoryDefaultProviderInitialized = false;
 
-	@Override
-	public TwoMotors get() {
-		if (!Boolean.getBoolean("fakeMotors")) {
-			initializeGpioFactoryDefaultProvider();
-			Motor motor1 = new GpioMotor(RaspiBcmPin.GPIO_12, RaspiBcmPin.GPIO_24, RaspiBcmPin.GPIO_22);
-			Motor motor2 = new ReverseMotor(new GpioMotor(RaspiBcmPin.GPIO_13, RaspiBcmPin.GPIO_25, RaspiBcmPin.GPIO_23));
-			return new TwoMotors(motor1, motor2);
-		} else {
-			return new TwoMotors(new LoggingMotor("left"), new LoggingMotor("right"));
-		}
-	}
+    // @Override
+    public TwoMotors get() {
+        if (!Boolean.getBoolean("fakeMotors")) {
+            initializeGpioFactoryDefaultProvider();
+            Motor motor1 = new GpioMotor(RaspiBcmPin.GPIO_12, RaspiBcmPin.GPIO_24, RaspiBcmPin.GPIO_22);
+            Motor motor2 = new ReverseMotor(
+                    new GpioMotor(RaspiBcmPin.GPIO_13, RaspiBcmPin.GPIO_25, RaspiBcmPin.GPIO_23));
+            return new TwoMotors(motor1, motor2);
+        } else {
+            return new TwoMotors(new LoggingMotor("left"), new LoggingMotor("right"));
+        }
+    }
 
-	private void initializeGpioFactoryDefaultProvider() {
-		if (!gpioFactoryDefaultProviderInitialized) {
-			GpioFactory.setDefaultProvider(new RaspiGpioProvider(RaspiPinNumberingScheme.BROADCOM_PIN_NUMBERING));
-			gpioFactoryDefaultProviderInitialized = true;
-		}
-	}
+    private void initializeGpioFactoryDefaultProvider() {
+        if (!gpioFactoryDefaultProviderInitialized) {
+            GpioFactory.setDefaultProvider(new RaspiGpioProvider(RaspiPinNumberingScheme.BROADCOM_PIN_NUMBERING));
+            gpioFactoryDefaultProviderInitialized = true;
+        }
+    }
 
 }
